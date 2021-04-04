3289c2e0d03895990aa006059383a51c6056fa24
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-04-04 14:10:42 -0400

    config comment changes

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 # Bot settings
-groupme_access_token = "" # Access token from GroupMe
+groupme_access_token = "" # Access token from GroupMe https://dev.groupme.com/
 
 botname = "FinBot" # Name of the bot as it will appear in the groupchat
 bot_char = "?" # Character to start messages that will be interpretted by the bot
 
 group_id = "" # Groupchat id 
-callback_url="" # URL that will get called every time a message is sent in the groupchat
+callback_url="" # URL that will get called every time a message is sent in the groupchat in the form of https://url.com/financialadvisors
+# If you know how to change the route go for it. I may change this in the future, but I don't feel like updating my personal bot.
 
 # API Keys
 av_api_key = "" # AlphaVantage API key https://www.alphavantage.co/support/#api-key
