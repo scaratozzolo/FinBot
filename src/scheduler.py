f632d1993b43cc5027b6bb563ddd115352562b5b
Scott Caratozzolo <scaratozzolo12@gmail.com>
2024-04-26 17:07:49 -0400

    create folder for scheduler funcs

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,50 +1,8 @@
-from datetime import datetime, date, time
-from zoneinfo import ZoneInfo
-import emoji
-from loguru import logger
 from apscheduler.schedulers.background import BackgroundScheduler
-from src.commands.quote import get_quote
-from src.utils import bot, finnhub_client
+from src.schedules.swings import check_swings
+from src.schedules.earnings import get_upcoming_earnings
 
-
-MARKET_OPEN = time(hour=9, minute=30, tzinfo=ZoneInfo("America/New_York"))
-MARKET_CLOSE = time(hour=16, tzinfo=ZoneInfo("America/New_York"))
-
-
-watchlist = ["BTC-USD", "LTC-USD", "NVDA", "SPY", "QQQ"]
-todays_alerts = {}
-
-def check_swings():
-    logger.info("Checking swings...")
-    global todays_alerts
-    swings = []
-
-    market_status = finnhub_client.market_status(exchange='US')
-
-    if market_status['isOpen']:
-        logger.debug("market is open")
-        for ticker in watchlist:
-            quote = finnhub_client.quote(ticker)
-            if quote['dp'] is None:
-                logger.warning(f"Ticker not found: {ticker=}")
-            
-            if abs(quote['dp']) > 1:
-                logger.debug(f"{ticker=}, {quote['dp']=}")
-                swings.append(ticker)
-
-        to_send = [f"${i}" for i in swings if f"${i}" not in todays_alerts]
-        logger.debug(f"{to_send=}")
-        if len(to_send) > 0:
-            bot.post(emoji.emojize(":police_car_light: Swing Alert :police_car_light:"))
-            get_quote(" ".join(to_send))
-            for i in to_send:
-                todays_alerts[i] = datetime.now()
-
-    if not market_status['isOpen']:
-        logger.debug("market is closed")
-        todays_alerts = {}
-
-    logger.debug(f"{todays_alerts=}")
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(check_swings, 'interval', minutes=30)
+scheduler.add_job(get_upcoming_earnings, 'cron', day_of_week=6, hour=9)
---
+++ src/schedules/earnings.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+import emoji
+from loguru import logger
+from src.utils import bot, finnhub_client
+from src.watchlist import watchlist
+
+def get_upcoming_earnings():
+    logger.info("Getting earnings")
+
+    from_date = datetime.now(tz=ZoneInfo("America/New_York"))
+    from_date_str = from_date.strftime("%Y-%m-%d")
+    to_date = from_date + timedelta(days=7)
+    to_date_str = to_date.strftime("%Y-%m-%d")
+    logger.debug(f"checking earnings {from_date=} {to_date=}")
+
+    msg = emoji.emojize("Upcoming Earnings :calendar:\n\n")
+    earnings_cal = {}
+    send_message = False
+    for ticker in watchlist:
+        cal = finnhub_client.earnings_calendar(_from=from_date_str, to=to_date_str, symbol=ticker)["earningsCalendar"]
+        if cal != []:
+            logger.debug(f"{ticker} has earnings")
+            send_message = True
+            earnings_date = datetime.strptime(cal[0]["date"], "%Y-%m-%d")
+            logger.debug(f"{ticker} {earnings_date=}")
+            # Looks like data being returned in inaccurate
+            # if cal[0]["hour"] == "amc":
+            #     when = "After Market Close"
+            # elif cal[0]["hour"] == "bmo":
+            #     when = "Before Market Open"
+            # else:
+            #     when = ""
+            when = ""
+            logger.debug(f"{ticker} {when=}")
+
+            ticker_msg = ticker +"\n"
+            ticker_msg += f"{earnings_date.strftime('%B %d, %Y')} {when}\n\n"
+
+            earnings_cal[ticker] = {"date": earnings_date, "msg": ticker_msg}
+
+    if send_message:
+        logger.info("Sending upcoming earnings")
+        earnings_cal = {k: v for k, v in sorted(earnings_cal.items(), key=lambda item: item[1]['date'])}
+        for _, i in earnings_cal.items():
+            msg += i['msg']
+        bot.post(msg)
+    else:
+        logger.debug("no earnings to send")
+
+
+
