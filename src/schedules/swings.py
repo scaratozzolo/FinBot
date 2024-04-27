275e06fb80a3f993e6c0da3f028acddaa94cec00
Scott Caratozzolo <scott@caratozzolo.xyz>
2024-04-27 13:05:32 -0400

    fix logic

--- src/schedules/swings.py
+++ src/schedules/swings.py
@@ -29,19 +29,19 @@ def check_swings():
                 continue
 
             result = todays_alerts.find_one({"ticker": ticker})
-            
-            if result is None and abs(quote['dp'] > 1):
-                logger.debug(f"{ticker=}, {quote['dp']=}")
+            logger.debug(f"{ticker=}, {quote['dp']=}, {result=}")
+            if result is None and abs(quote['dp']) > 1:
+                logger.info(f"swing true for {ticker}")
                 swings.append({"ticker":ticker, "percent":quote['dp']})
             elif result is not None and abs(quote['dp']-result['percent']) > 1:
-                logger.debug(f"{ticker=}, {quote['dp']=}")
+                logger.info(f"swing true for {ticker}")
                 swings.append({"ticker":ticker, "percent":quote['dp']})
 
         logger.debug(f"{swings=}")
         if len(swings) > 0:
             bot.post(emoji.emojize(":police_car_light: Swing Alert :police_car_light:"))
             for i in swings:
-                get_quote(i['ticker'])
+                get_quote("$" + i['ticker'])
                 result = todays_alerts.insert_one({"ticker": i['ticker'], "alert_time": datetime.now(ZoneInfo("America/New_York")), "percent": i['percent']})
                 logger.debug(f"{result}")
 
@@ -49,5 +49,3 @@ def check_swings():
         logger.debug("market is closed")
         result = todays_alerts.delete_many({})
         logger.debug(f"{result}")
-
-    logger.debug(f"{todays_alerts=}")
