9ab7627c4d5411b8ba6147154f6c3940b51be2de
Scott Caratozzolo <scott@caratozzolo.xyz>
2024-04-28 20:57:36 -0400

    update logic

--- src/schedules/swings.py
+++ src/schedules/swings.py
@@ -16,9 +16,10 @@ watchlist = get_watchlist()
 def check_swings():
     logger.info("Checking swings...")
     todays_alerts = mongo_db['swing_alerts']
-    swings = []
 
     market_status = finnhub_client.market_status(exchange='US')
+
+    swing_alert_sent = False
 
     if not market_status['isOpen']:
         logger.debug("market is open")
@@ -30,19 +31,13 @@ def check_swings():
 
             result = todays_alerts.find_one({"ticker": ticker})
             logger.debug(f"{ticker=}, {quote['dp']=}, {result=}")
-            if result is None and abs(quote['dp']) > 1:
+            if (result is None and abs(quote['dp']) > 1) or (result is not None and abs(quote['dp']-result['percent']) > 1):
                 logger.info(f"swing true for {ticker}")
-                swings.append({"ticker":ticker, "percent":quote['dp']})
-            elif result is not None and abs(quote['dp']-result['percent']) > 1:
-                logger.info(f"swing true for {ticker}")
-                swings.append({"ticker":ticker, "percent":quote['dp']})
-
-        logger.debug(f"{swings=}")
-        if len(swings) > 0:
-            bot.post(emoji.emojize(":police_car_light: Swing Alert :police_car_light:"))
-            for i in swings:
-                get_quote("$" + i['ticker'])
-                result = todays_alerts.insert_one({"ticker": i['ticker'], "alert_time": datetime.now(ZoneInfo("America/New_York")), "percent": i['percent']})
+                if not swing_alert_sent:
+                    bot.post(emoji.emojize(":police_car_light: Swing Alert :police_car_light:"))
+                    swing_alert_sent = True
+                get_quote("$" + ticker)
+                result = todays_alerts.update_one({"ticker": ticker}, {"alert_time": datetime.now(ZoneInfo("America/New_York")), "percent": quote['dp']}, upsert=True)
                 logger.debug(f"{result}")
 
     if market_status['isOpen']:
