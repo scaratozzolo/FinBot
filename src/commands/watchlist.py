b31e9caad023ee001d60505fe60462fa07788274
Scott Caratozzolo <scott@caratozzolo.xyz>
2024-05-05 20:13:41 -0400

    add company name to message on watchlist add

--- src/commands/watchlist.py
+++ src/commands/watchlist.py
@@ -1,4 +1,5 @@
 import emoji
+import yfinance as yf
 from loguru import logger
 from src.watchlist import add_watchlist, remove_watchlist, get_watchlist
 from src.models import Commands
@@ -18,14 +19,23 @@ def handle_watchlist(msg, user_info):
         bot.post(reply_msg)
     elif len(msg_split) == 3:
         ticker = msg_split[2].upper()
+
         if msg_split[1].lower() == "add":
             logger.info("adding to watchlist")
             add_watchlist(ticker, user_info)
-            bot.post(emoji.emojize(f":plus: {ticker} added to watchlist."))
+            try:
+                yf_ticker = yf.Ticker(ticker).info
+                ticker_name = yf_ticker["shortName"]
+                bot.post(emoji.emojize(f":plus: {ticker_name} ({ticker}) added to watchlist."))
+            except Exception as excp:
+                logger.error(excp)
+                bot.post(emoji.emojize(f":plus: {ticker} added to watchlist."))
+
         elif msg_split[1].lower() == "remove":
             logger.info("removing from watchlist")
             remove_watchlist(ticker)
             bot.post(emoji.emojize(f":minus: {ticker} removed from watchlist."))
+
         else:
             bot.post(Commands.WATCHLIST.value.usage)
 
