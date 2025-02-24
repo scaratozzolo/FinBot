bd8a52b5a37f4d9d341d4c8a71fb46b5d6c1accd
Scott Caratozzolo <scott@caratozzolo.xyz>
2025-02-24 12:59:23 -0500

    simplify watchlist

--- src/watchlist.py
+++ src/watchlist.py
@@ -4,13 +4,14 @@ from src.db import mongo_db
 
 watchlist_collection = mongo_db["watchlist"]
 
+
 def get_watchlist():
     result = watchlist_collection.find().sort("ticker")
 
     return [i["ticker"] for i in result]
 
 
-def add_watchlist(ticker, user_info):
+def add_watchlist(ticker, user_info) -> bool:
     ticker_exists = watchlist_collection.find_one({"ticker": ticker})
     logger.debug(f"{ticker_exists=}")
 
@@ -23,9 +24,17 @@ def add_watchlist(ticker, user_info):
         )
 
         logger.debug(f"{result}")
+        return True
+
+    return False
 
 
-def remove_watchlist(ticker):
+def remove_watchlist(ticker) -> bool:
     result = watchlist_collection.delete_one({"ticker": ticker})
 
     logger.debug(f"{result}")
+
+    if result.deleted_count == 1:
+        return True
+
+    return False
